3d52e41772b24087282408461ce025311d4be2cb
andrew-helms <helmsandrew3@gmail.com>
2017-11-17 15:06:38 -0500

    Fixed player class

--- src/Player.h
+++ src/Player.h
@@ -2,17 +2,18 @@
 #define PLAYER_H
 
 #include "Piece.h"
+#include "Card.h"
 
 class Player {
 private:
 	int score;
-	Piece ***playerPieces [2][16];
+	Piece ***playerPieces [16];
 
 public:
 	Player(bool isHuman);
 	int getScore();
+	void setScore(int scoreDiff);
 	void playCard(int index);
-	void setScore();
 	void printCards();
 };
 
